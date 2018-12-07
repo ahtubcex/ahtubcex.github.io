f35914edd9786c4a5fa69066bf582cdd3ad2575f
Ibragim <ahtubcex@ya.ru>
2018-12-07 16:38:55 +0300

    changed smth

--- js/index.js
+++ js/index.js
@@ -68,6 +68,22 @@
         }
     }
 
+    function downloadFile(url){
+  var link = document.createElement('a');
+  link.href = url;
+  link.target="_blank";
+  link.download='';
+  
+  var event = document.createEvent("MouseEvents");
+  event.initMouseEvent("click");
+  link.dispatchEvent(event);
+}
+
+var but = document.getElementById('dbut')
+but.addEventListener('click', function(){
+  downloadFile(but.getAttribute('data-path'));
+})
+
     // Event handling
     function addListeners() {
         if(!('ontouchstart' in window)) {

0e8291b8c819de9a50771ee6185bed4ca9fc8a4f
Ibragim <ahtubcex@ya.ru>
2019-02-24 23:48:35 +0300

    edited js file

--- js/index.js
+++ js/index.js
@@ -87,7 +87,7 @@ but.addEventListener('click', function(){
     // Event handling
     function addListeners() {
         if(!('ontouchstart' in window)) {
-            // window.addEventListener('mousemove', mouseMove);
+            window.addEventListener('mousemove', mouseMove);
         }
         window.addEventListener('scroll', scrollCheck);
         window.addEventListener('resize', resize);
